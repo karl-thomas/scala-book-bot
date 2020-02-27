d34220b30ffc5b209d2b6a1382562a9259e8e5e5
Karl Thomas <karl.thomas925@gmail.com>
2020-02-27 16:35:14 -0600

    handle grabbing the env variable better

--- src/main/scala/example/BookBot.scala
+++ src/main/scala/example/BookBot.scala
@@ -4,14 +4,13 @@ import scalaj.http._
 import io.circe._, io.circe.parser._
 
 object BookBot extends App {
-
   def parseJson(json: String): Json = {
     parse(json).getOrElse(Json.Null)
   }
 
   def getBook(title: String): String = {
     val response: HttpResponse[String] = Http("https://www.googleapis.com/books/v1/volumes")
-      .param("key", sys.env("GOOGLE_API_KEY"))
+      .param("key", sys.env.getOrElse("GOOGLE_API_KEY", ""))
       .param("q", s"intitle:${title}")
       .asString
     response.body
