d1f35e8b06d099ca99ffab25b0fa348604b74d54
Karl Thomas <karl.thomas925@gmail.com>
2020-03-19 22:57:36 -0500

    rename pingback model to be more domain specific

--- src/main/scala/bookbot/PingHandler.scala
+++ src/main/scala/bookbot/PingHandler.scala
@@ -5,16 +5,16 @@ import cats.implicits._
 import io.circe.{Encoder, Json}
 import org.http4s.EntityEncoder
 import org.http4s.circe._
-import bookbot.models.PingBack
+import bookbot.models.MessageToSlackChannel
 
 trait PingHandler[F[_]]{
-  def pingback(): F[PingBack]
+  def pingback(): F[MessageToSlackChannel]
 }
 
 object PingHandler {
   implicit def apply[F[_]](implicit ev: PingHandler[F]): PingHandler[F] = ev
   
   def impl[F[_]: Applicative]: PingHandler[F] = new PingHandler[F]{
-    def pingback: F[PingBack] = PingBack("pingback").pure[F]
+    def pingback: F[MessageToSlackChannel] = MessageToSlackChannel("pingback").pure[F]
   }
 }
